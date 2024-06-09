bba7b49469575cd29eb4f46802340112d2d92c1e
Oliwier Juncewicz <oli.junc@gmail.com>
2024-06-10 00:04:47 +0200

    Made changes do ParkGuideViewModel.kt. Implemented Park and Ui event handling. Also added logs for it

--- app/src/main/java/com/junclabs/parkguide/ui/ParkGuideViewModel.kt
+++ app/src/main/java/com/junclabs/parkguide/ui/ParkGuideViewModel.kt
@@ -1,19 +1,63 @@
 package com.junclabs.parkguide.ui
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.junclabs.parkguide.data.State
+import androidx.lifecycle.viewModelScope
+import com.junclabs.parkguide.ParkEvent
+import com.junclabs.parkguide.Screen
 import com.junclabs.parkguide.data.DataSource
-import com.junclabs.parkguide.data.Park
+import com.junclabs.parkguide.util.UiEvent
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class ParkGuideViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(UiState())
     val uiState: StateFlow<UiState> = _uiState
 
+    private val _uiEvent = Channel<UiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
     init {
         initializeUiState()
+    }
+
+    fun onEvent(event: ParkEvent) {
+        when (event) {
+            is ParkEvent.OnCurrentParkUpdate -> {
+                _uiState.update {
+                    it.copy(
+                        currentPark = event.park
+                    )
+                }
+                Log.d("ParkGuideViewModel", "OnCurrentParkUpdate: ${event.park}")
+            }
+
+            is ParkEvent.OnCurrentStateUpdate -> {
+                _uiState.update {
+                    it.copy(
+                        currentState = event.state
+                    )
+                }
+                Log.d("ParkGuideViewModel", "OnCurrentStateUpdate: ${event.state}")
+            }
+
+            is ParkEvent.OnCurrentParkClick -> {
+                sendUiEvent(UiEvent.Navigate(Screen.DetailPark.name))
+            }
+
+            is ParkEvent.OnCurrentStateClick -> {
+                sendUiEvent(UiEvent.Navigate(Screen.Parks.name))
+            }
+
+            is ParkEvent.OnNavigateBack -> {
+                sendUiEvent(UiEvent.PopBackStack(event.route, event.inclusive))
+            }
+        }
     }
 
     private fun initializeUiState() {
@@ -22,19 +66,9 @@ class ParkGuideViewModel : ViewModel() {
         )
     }
 
-    fun updateCurrentState(state: State) {
-        _uiState.update {
-            it.copy(
-                currentState = state
-            )
-        }
-    }
-
-    fun updateCurrentPark(park: Park?) {
-        _uiState.update {
-            it.copy(
-                currentPark = park
-            )
+    private fun sendUiEvent(event: UiEvent) {
+        viewModelScope.launch(Dispatchers.IO) {
+            _uiEvent.send(event)
         }
     }
 }
