53c49843109bcef05cca24603a52cf05aa8e8542
Oliwier Juncewicz <oli.junc@gmail.com>
2024-06-13 16:23:36 +0200

    Added LaunchedEffect block with UiEvents, made text composables use textStyle. onNavigationClick now uses viewModel.onEvent.

--- app/src/main/java/com/junclabs/parkguide/ui/DetailParkScreen.kt
+++ app/src/main/java/com/junclabs/parkguide/ui/DetailParkScreen.kt
@@ -15,34 +15,46 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.junclabs.parkguide.R
 import com.junclabs.parkguide.data.Park
 import com.junclabs.parkguide.ui.theme.ParkGuideTheme
 import com.junclabs.parkguide.util.AppBar
+import com.junclabs.parkguide.util.UiEvent
+import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun DetailParkScreen(
     modifier: Modifier = Modifier,
-    onNavigateBack: () -> Unit,
+    onNavigateUp: (UiEvent.NavigateUp) -> Unit,
+    viewModel: ParkGuideViewModel,
     uiState: UiState,
 ) {
+    LaunchedEffect(Unit) {
+        viewModel.uiEvent.collectLatest { event ->
+            when (event) {
+                is UiEvent.NavigateUp -> {
+                    onNavigateUp(event)
+                }
+
+                else -> Unit
+            }
+        }
+    }
     val scrollState = rememberScrollState()
     Scaffold(topBar = {
         AppBar(
             title = stringResource(id = R.string.learnMore),
-            onNavigationIconClick = onNavigateBack,
+            onNavigationIconClick = { viewModel.onEvent(ParkEvent.OnNavigateUp) },
             navigateBack = true
         )
     }) { innerPadding ->
@@ -71,10 +83,7 @@ fun DetailParkScreen(
                     ?: stringResource(
                         id = R.string.defaultParkTitle
                     ),
-                    textAlign = TextAlign.Center,
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.W700,
-                    lineHeight = 28.sp,
+                    style = MaterialTheme.typography.titleMedium,
                     modifier = modifier.padding(horizontal = 6.dp))
                 uiState.currentPark?.image?.let { painterResource(id = it) }?.let {
                     Image(
@@ -90,9 +99,7 @@ fun DetailParkScreen(
                     ?: stringResource(
                         id = R.string.defaultParkDescription
                     ),
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.W500,
-                    lineHeight = 28.sp,
+                    style = MaterialTheme.typography.bodyMedium,
                     modifier = modifier.padding(horizontal = 6.dp))
             }
         }
@@ -101,11 +108,11 @@ fun DetailParkScreen(
 
 @Composable
 @PreviewLightDark
-@Preview
 fun DetailParkScreenPreview() {
+    val viewModel: ParkGuideViewModel = viewModel()
     ParkGuideTheme {
         DetailParkScreen(
-            onNavigateBack = { }, uiState = UiState(
+            onNavigateUp = { }, viewModel = viewModel, uiState = UiState(
                 currentPark = Park(
                     title = R.string.Arches_National_Park,
                     description = R.string.Arches_National_Park_Description,
