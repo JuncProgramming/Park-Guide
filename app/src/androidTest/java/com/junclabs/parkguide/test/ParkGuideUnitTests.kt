bcadcf563a5f1ebf651d8263b0446f0ed17b5227
Oliwier Juncewicz <oli.junc@gmail.com>
2024-06-20 18:22:28 +0200

    Fixed naming in ParkGuideUnitTests.kt and changed the other.xml

--- app/src/androidTest/java/com/junclabs/parkguide/test/ParkGuideUnitTests.kt
+++ app/src/androidTest/java/com/junclabs/parkguide/test/ParkGuideUnitTests.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.test.platform.app.InstrumentationRegistry
 import com.junclabs.parkguide.R
-import com.junclabs.parkguide.data.State
 import com.junclabs.parkguide.data.Park
+import com.junclabs.parkguide.data.State
 import com.junclabs.parkguide.ui.DetailParkScreen
+import com.junclabs.parkguide.ui.ParkGuideViewModel
 import com.junclabs.parkguide.ui.UiState
 import com.junclabs.parkguide.ui.theme.ParkGuideTheme
 import org.junit.Rule
@@ -19,7 +21,7 @@ class ParkGuideUnitTests {
     val composeTestRule = createComposeRule()
 
     @Test
-    fun placeScreenContentAppearsCorrectly() {
+    fun detailParkScreenContentAppearsCorrectly() {
         val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
         val olympicTitleString = context.resources.getString(R.string.Olympic_National_Park)
         val olympicDescriptionString =
@@ -51,9 +53,10 @@ class ParkGuideUnitTests {
             )
         )
         composeTestRule.setContent {
+            val viewModel: ParkGuideViewModel = viewModel()
             ParkGuideTheme {
                 DetailParkScreen(
-                    onNavigateBack = {}, uiState = UiState(
+                    onNavigateUp = {}, viewModel = viewModel, uiState = UiState(
                         states = states, currentPark = Park(
                             title = R.string.Olympic_National_Park,
                             description = R.string.Olympic_National_Park_Description,
